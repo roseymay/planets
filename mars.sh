2eb831bb226a6f7ba3b2fb4dbd9033ebd1647722
roseymay <rosemaymclellan@gmail.com>
2022-10-17 16:17:16 +1300

    adding to test a push remote

--- mars.sh
+++ mars.sh
@@ -3,3 +3,5 @@
 echo "Cold and dry, but everything is my favourite colour"
 echo  "The two moons may be  a problem for Wolfman"
 echo "But the mummy will appreicate the lack of humidity"
+echo "rubbish"
+
